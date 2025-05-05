7c828b422a3d9e4f5703a8ef2e5827440202ba4a
Matteo <mrtn.matteo@proton.me>
2025-05-05 23:02:28 +0200

    corrected log display logic

--- ui.lua
+++ ui.lua
@@ -12,19 +12,13 @@ love.graphics.setDefaultFilter("nearest", "nearest")
 local logFont = love.graphics.newFont("fonts/cour.ttf", 24)
 love.graphics.setFont(logFont)
 
-local function drawLogLine(n, text)
-    local x = 10
-    local y = 40
-    love.graphics.print(text, x, y * n)
-end
-
 local UI = {
     displayLog = function()
         local entriesToPrint = Utils.lastUpToN(log.logs, 10)
-        for i = 1, #entriesToPrint, 1 do
-            love.graphics.setColor(1, 1, 1)
-            drawLogLine(i, entriesToPrint[i])
-        end
+        local x = 10
+        local y = 40
+        local limit = 400
+        love.graphics.printf(table.concat(entriesToPrint, '\n'), x, y, 400)
     end
 }
 

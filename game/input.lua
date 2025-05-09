9c59a252a589b2ea4dffcd63c45abda41c096d53
Matteo <mrtn.matteo@proton.me>
2025-05-09 11:08:13 +0200

    disabled unused keybindings

--- game/input.lua
+++ game/input.lua
@@ -11,7 +11,7 @@ function love.keypressed(key, scancode, isrepeat)
     elseif key == "down" then
         Camera.y = Camera.y + Camera.speed
     end ]]
-    if key == "d" then
+--[[     if key == "d" then
         relativeRotation = relativeRotation + rotationSpeed
     end
     if key == "f" then
@@ -23,5 +23,5 @@ function love.keypressed(key, scancode, isrepeat)
     end
     if key == "e" then
         radiusRatio = math.max(0.1, radiusRatio - 0.02)
-    end
+    end ]]
 end
